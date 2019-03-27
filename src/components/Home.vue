3f983c2c60778d71a61cd5c27d54d5b5abf97fa2
364864264 <364864264sun>
2019-03-27 21:23:12 +0800

    提交

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,42 @@
 <template>
     <div class="container">
+        <button type="button" class="btn btn-default" @click="btn">（默认样式）Default</button>
 
     </div>
 </template>
 
 <script>
-	export default {
-		name: "Home",
-		data () {
-			return {
-				message: "传入的值"
-			}
-		},
-		mounted () {
+  export default {
+    name: "Home",
+    data () {
+      return {
+        message: "传入的值"
+      }
+    },
+    mounted () {
 
-		},
-		methods: {
-
-		},
-		components: {}
-	}
+    },
+    methods: {
+      btn () {
+        this.layer.alert(res.msg, {
+          type: 0, //0（信息框，默认）1（页面层）2（iframe层）3（加载层）4（tips层）
+          title: '信息',
+          content: '',
+          area: 'auto',
+          offset: 'auto',
+          icon: -1,
+          btn: '确定',
+          time: 0,
+          shade: true,//是否显示遮罩
+          yes: '',
+          cancel: '',
+          tips: [0, {}],//支持上右下左四个方向，通过1-4进行方向设定,可以设定tips: [1, '#c00']
+          tipsMore: false,//是否允许多个tips
+          shadeClose: false,//点击遮罩是否关闭
+        })
+      }
+    },
+    components: {}
+  }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,21 +2,23 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import "animate.css/animate.min.css"
 import "bootstrap/dist/css/bootstrap.min.css"
-import "@~/js/bootstrap"
+import "../static/css/main.scss"
+import "@@/js/bootstrap"
 import layer from "vue-layer"
 import Vue from "vue"
 import App from "./App"
 import router from "./router"
-import "../static/css/main.css"
 
-Vue.prototype.layer = layer(Vue)
+Vue.prototype.layer = layer(Vue, {
+  msgtime: 2
+})
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-	el: "#app",
-	router,
-	components: {App},
-	template: "<App/>"
+  el: "#app",
+  router,
+  components: {App},
+  template: "<App/>"
 })
