3d31b8d4e036eb12571c148387bb1f2d3445cf89
364864264 <364864264@qq.com>
2019-06-04 13:32:43 +0800

    提交

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="container">
-        <h1>网站建设中...</h1>
+        <a href="javascript:void(0);" class="linkCss3">css3</a>
     </div>
 </template>
 
 <script>
-    import "@~/js/VeeValidateConfig"
+	import "@~/js/VeeValidateConfig"
 
 	export default {
 		name: "Home",
@@ -17,9 +17,29 @@
 		mounted () {
 
 		},
-		methods: {
-        },
+		methods: {},
 		components: {}
 	}
 </script>
+<style lang="scss">
+    .linkCss3 {
+        width:200px;height:40px;line-height:40px;border:1px solid #ededed;display:block;position:relative;
+        &:after {
+            content:'';width:0;height:0;position:absolute;border:1px solid transparent;box-sizing:border-box;
+            transition:width 0.2s ease-out, height 0.2s ease-out 0.2s;
+        }
+        &:before {
+            content:'';width:0;height:0;position:absolute;border:1px solid transparent;box-sizing:border-box;
+            transition:border-color 0s ease-out 0.4s, width 0.2s ease-out, height 0.2s ease-out 0.6s;right:0;bottom:0;
+        }
+        &:hover {
+            &:before {
+                /*width:100%;height:100%;border-top-color:#f00;border-right-color:#f00;*/
+            }
+            &:after {
+                width:100%;height:100%;border-bottom-color:#f00;border-left-color:#f00;
+            }
+        }
+    }
+</style>
 
