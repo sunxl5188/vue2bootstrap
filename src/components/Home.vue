ba20bd2b38d242f57d908dc37615a1b29fe50188
364864264 <364864264@qq.com>
2019-08-21 18:01:12 +0800

    提交

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,22 @@
 <template>
-    <div class="container text-right">
+    <div class="container">
         <a class="btn btn-primary" role="button" tabindex="0">可消失的弹出框</a>
+        <form action="" method="POST" class="form-horizontal" @submit.prevent="validateBeforeSubmit">
+             <div class="form-group">
+                <label class="col-sm-2 control-label">所在地区</label>
+                <div class="col-sm-10">
+                    <CitySelect></CitySelect>
+                </div>
+            </div>
+
+        </form>
+
     </div>
 </template>
 
 <script>
 	import "@~/js/VeeValidateConfig"
+	import CitySelect from "@#/shared/CitySelect"
 
 	export default {
 		name: "Home",
@@ -15,17 +26,17 @@
 			}
 		},
 		mounted () {
-            setTimeout(function () {
-							$(".btn-primary").popover({
-								html:true,
-								trigger:"focus",
-								placement:"auto",
-								content:"<dd><a href='#'>创建提醒</a></dd><dd><a href='#'>创建日程</a></dd>"
-							})
-            }, 1000)
+			setTimeout(function () {
+				$(".btn-primary").popover({
+					html: true,
+					trigger: "focus",
+					placement: "auto",
+					content: "<dd><a href='#'>创建提醒</a></dd><dd><a href='#'>创建日程</a></dd>"
+				})
+			}, 1000)
 		},
 		methods: {},
-		components: {}
+		components: {CitySelect}
 	}
 </script>
 <style lang="scss">
---
+++ src/components/shared/CitySelect.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="form-inline">
+        <select name="" class="form-control">
+            <option value=""> 请选择</option>
+        </select>
+        <select name="" class="form-control">
+            <option value=""> 请选择</option>
+        </select>
+        <select name="" class="form-control">
+            <option value=""> 请选择</option>
+        </select>
+    </div>
+</template>
+
+<script>
+	import cityList from "@@/json/cityjson"
+
+	export default {
+		name: "city",
+		data () {
+			return {}
+		},
+		computed: {},
+		mounted () {
+			let self = this
+		},
+		methods: {},
+		components: {},
+		watch: {},
+		errorCaptured (err, vm, info) {}
+	}
+</script>
+
+<style scoped>
+
+</style>
